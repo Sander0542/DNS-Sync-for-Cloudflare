7abfa8a68a97e19caf5b2456b0b8d59d9b167df7
Sander Jochems <sanderjochems@hotmail.nl>
2019-06-12 23:51:07 +0200

    Create Cloudflare Record Helper Class

---
+++ plib/library/Cloudflare/Record.php
@@ -0,0 +1,19 @@
+<?php
+
+class Modules_DnsSyncCloudflare_Cloudflare_Record
+{
+    public $id;
+    public $type;
+    public $name;
+    public $content;
+    public $proxiable;
+    public $proxied;
+    public $ttl;
+    public $locked;
+    public $zone_id;
+    public $zone_name;
+    public $created_on;
+    public $modified_on;
+    public $data;
+    public $priority;
+}
