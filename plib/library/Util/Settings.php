d001fccb46f87fab8487ff4f657406ed2e57dbe8
Sander Jochems <sanderjochems@hotmail.nl>
2019-06-12 23:51:26 +0200

    Create Settings Util Class

---
+++ plib/library/Util/Settings.php
@@ -0,0 +1,23 @@
+<?php
+
+class Modules_DnsSyncCloudflare_Util_Settings
+{
+    const CLOUDFLARE_EMAIL = 'cloudflareEmail';
+    const CLOUDFLARE_API_KEY = 'cloudflareApiKey';
+
+    const CLOUDFLARE_PROXY = 'cloudflareProxy';
+    const CLOUDFLARE_SYNC_TYPES = 'cloudflareSyncTypes';
+    const CLOUDFLARE_AUTO_SYNC = 'cloudflareAutomaticSync';
+
+    const CLOUDFLARE_DOMAIN_USER = 'cloudflareDomainUser';
+
+    public static function getUserKey($key, $userID = null)
+    {
+        return 'u'.(is_numeric($userID) ? $userID : pm_Session::getClient()->getId()).'_'.$key;
+    }
+
+    public static function getDomainKey($key, $site_id)
+    {
+        return 'd'.$site_id.'_'.$key;
+    }
+}
