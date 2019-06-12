c48f63a0581273698da3e5ed8a3c41ab6f02f162
Sander Jochems <sanderjochems@hotmail.nl>
2019-06-12 23:51:35 +0200

    Create Some Translations

---
+++ plib/resources/locales/en-US.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Created by Sander Jochems
+ */
+
+$messages = [
+    'title' => 'DNS Sync for Cloudflare®',
+    'description' => 'Sync the Plesk DNS to Cloudflare DNS',
+
+    'title.dnsSyncFor' => 'DNS Sync for <b>%%domain%%</b>',
+
+    'tab.records' => 'Records',
+    'tab.api' => 'API',
+    'tab.settings' => 'Settings',
+
+    'form.cloudflareEmail' => 'Cloudflare Email',
+    'form.cloudflareApiKey' => 'Cloudflare API Key',
+
+    'form.trafficThruCloudflare' => 'Traffic Thru Cloudflare',
+    'form.automaticSync' => 'Sync DNS automatic',
+    'form.selectRecord' => 'Select the type of records you want to sync',
+
+    'table.domainName' => 'Domain Name',
+    'table.cloudflareZoneID' => 'Cloudflare Zone ID',
+
+    'table.host' => 'Cloudflare Zone ID',
+    'table.recordType' => 'Record Type',
+    'table.status' => 'Status',
+    'table.cloudflareValue' => 'Cloudflare Value',
+    'table.pleskValue' => 'Plesk Value',
+
+    'button.syncDNS' => 'Sync DNS',
+    'button.save' => 'Save',
+
+    'text.zoneIdNotFound' => 'Zone ID not found',
+    'text.recordNotFound' => 'Record not found',
+
+    'message.apiSaved' => 'API Settings were successfully saved.',
+    'message.settingsSaved' => 'Domain Settings were successfully saved.',
+    'message.noConnection' => 'Could not connect to Cloudflare.',
+    'message.couldNotSync' => 'Could not sync the Plesk DNS zone to Cloudflare.',
+    'message.noCloudflareZoneFound' => 'Could not find a Cloudflare zone for this domain.',
+    'message.noAccessToDomain' => 'You do not have access to this domain.',
+    'message.noAccessExtension' => 'You do not have access to DNS Sync for Cloudflare.',
+    'message.noAccessSettings' => 'You do not have access to the domain settings.',
+    'message.noDomainSelected' => 'There was no domain selected.',
+    'message.noRecordsEdited' => 'No records created of updated.',
+    'message.xRecordsCreated' => '%%count%% record(s) created.',
+    'message.xRecordsUpdated' => '%%count%% record(s) updated.',
+
+    'permission.cloudflare.title' => 'DNS Sync for Cloudflare',
+    'permission.cloudflare.description' => 'Allow customers to use DNS Sync for Cloudflare',
+    'permission.cloudflareSettings.title' => 'DNS Sync for Cloudflare Settings',
+    'permission.cloudflareSettings.description' => 'Allow customers to change the settings of DNS Sync for Cloudflare',
+];
---
+++ plib/resources/locales/nl-NL.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Created by Sander Jochems
+ */
+
+$messages = [
+    'title' => 'DNS Sync voor Cloudflare®',
+    'description' => 'Synchroniseer de DNS van Plesk met Cloudflare DNS',
+
+    'title.dnsSyncFor' => 'DNS Sync voor <b>%%domain%%</b>',
+
+    'tab.domains' => 'Domeinen',
+    'tab.api' => 'API',
+    'tab.records' => 'Records',
+
+    'form.cloudflareEmail' => 'Cloudflare Email',
+    'form.cloudflareApiKey' => 'Cloudflare API Key',
+
+    'form.trafficThruCloudflare' => 'Verkeer door Cloudflare',
+    'form.automaticSync' => 'Synchroniseer DNS automatisch',
+    'form.selectRecord' => 'Selecteer het type records dat u wilt synchroniseren',
+
+    'table.domainName' => 'Domein Naam',
+    'table.cloudflareZoneID' => 'Cloudflare Zone ID',
+
+    'table.host' => 'Cloudflare Zone ID',
+    'table.recordType' => 'Record Type',
+    'table.status' => 'Status',
+    'table.cloudflareValue' => 'Cloudflare Waarde',
+    'table.pleskValue' => 'Plesk Waarde',
+
+    'button.syncDNS' => 'Synchroniseer DNS',
+    'button.save' => 'Opslaan',
+
+    'text.zoneIdNotFound' => 'Zone ID niet gevonden',
+    'text.recordNotFound' => 'Record niet gevonden',
+
+    'message.apiSaved' => 'API-instellingen zijn succesvol opgeslagen.',
+    'message.settingsSaved' => 'Domeininstellingen zijn succesvol opgeslagen.',
+    'message.noConnection' => 'Kon niet verbinden met Cloudflare.',
+    'message.couldNotSync' => 'De Plesk DNS-zone kon niet worden gesynchroniseerd met Cloudflare.',
+    'message.noCloudflareZoneFound' => 'Kan geen Cloudflare zone voor dit domein gevonden worden.',
+    'message.noAccessToDomain' => 'U heeft geen toegang tot dit domein.',
+    'message.noAccessExtension' => 'U heeft geen toegang tot DNS Sync voor Cloudflare.',
+    'message.noAccessSettings' => 'U heeft geen toegang tot de domein instellingen.',
+    'message.noDomainSelected' => 'Er was geen domein geselecteerd.',
+    'message.noRecordsEdited' => 'Geen records gemaakt of geupdate.',
+    'message.xRecordsCreated' => '%%count%% record(s) gemaakt.',
+    'message.xRecordsUpdated' => '%%count%% record(s) geupdate.',
+];
