6834a7b978fc75955a2764226dc9c0963193fdb4
Sander Jochems <sanderjochems@hotmail.nl>
2019-06-13 15:49:57 +0200

    Update DomainController

--- plib/controllers/DomainController.php
+++ plib/controllers/DomainController.php
@@ -86,7 +86,7 @@ class DomainController extends pm_Controller_Action
             $form = new pm_Form_Simple();
             $form->addElement('Text', Modules_DnsSyncCloudflare_Util_Settings::CLOUDFLARE_EMAIL, array(
                 'label' => pm_Locale::lmsg('form.cloudflareEmail'),
-                'value' => pm_Settings::getDecrypted(Modules_DnsSyncCloudflare_Util_Settings::getUserKey(Modules_DnsSyncCloudflare_Util_Settings::CLOUDFLARE_EMAIL)),
+                'value' => pm_Settings::getDecrypted(Modules_DnsSyncCloudflare_Util_Settings::getDomainKey(Modules_DnsSyncCloudflare_Util_Settings::CLOUDFLARE_EMAIL, $domain->getId())),
                 'required' => true,
                 'validator' => array(
                     array('EmailAddress', true)
@@ -94,7 +94,6 @@ class DomainController extends pm_Controller_Action
             ));
             $form->addElement('Text', Modules_DnsSyncCloudflare_Util_Settings::CLOUDFLARE_API_KEY, array(
                 'label' => pm_Locale::lmsg('form.cloudflareApiKey'),
-                'value' => pm_Settings::getDecrypted(Modules_DnsSyncCloudflare_Util_Settings::getUserKey(Modules_DnsSyncCloudflare_Util_Settings::CLOUDFLARE_API_KEY)),
                 'required' => true,
                 'validator' => array(
                     array('NotEmpty', true)
@@ -105,10 +104,10 @@ class DomainController extends pm_Controller_Action
             ));
 
             if ($this->getRequest()->isPost() && $form->isValid($this->getRequest()->getPost())) {
-                pm_Settings::setEncrypted(Modules_DnsSyncCloudflare_Util_Settings::getUserKey(Modules_DnsSyncCloudflare_Util_Settings::CLOUDFLARE_EMAIL), $form->getValue(Modules_DnsSyncCloudflare_Util_Settings::CLOUDFLARE_EMAIL));
-                pm_Settings::setEncrypted(Modules_DnsSyncCloudflare_Util_Settings::getUserKey(Modules_DnsSyncCloudflare_Util_Settings::CLOUDFLARE_API_KEY), $form->getValue(Modules_DnsSyncCloudflare_Util_Settings::CLOUDFLARE_API_KEY));
+                pm_Settings::setEncrypted(Modules_DnsSyncCloudflare_Util_Settings::getDomainKey(Modules_DnsSyncCloudflare_Util_Settings::CLOUDFLARE_EMAIL, $domain->getId()), $form->getValue(Modules_DnsSyncCloudflare_Util_Settings::CLOUDFLARE_EMAIL));
+                pm_Settings::setEncrypted(Modules_DnsSyncCloudflare_Util_Settings::getDomainKey(Modules_DnsSyncCloudflare_Util_Settings::CLOUDFLARE_API_KEY, $domain->getId()), $form->getValue(Modules_DnsSyncCloudflare_Util_Settings::CLOUDFLARE_API_KEY));
                 $this->_status->addMessage('info', pm_Locale::lmsg('message.apiSaved'));
-                $this->_helper->json(array('redirect' => pm_Context::getBaseUrl()));
+                $this->_helper->json(array('redirect' => pm_Context::getActionUrl('domain','records?site_id='.$domain->getId())));
             }
 
             $this->view->form = $form;
