0ee00dc76b499b1ba21fe20fcaa3daa75df2275a
Sander Jochems <sanderjochems@hotmail.nl>
2019-06-19 23:58:23 +0200

    Update CloudflareRecord

--- plib/library/Cloudflare/Record.php
+++ plib/library/Cloudflare/Record.php
@@ -1,5 +1,8 @@
 <?php
 
+use PleskX\Api\Struct\Dns\Info as PleskRecord;
+use Modules_DnsSyncCloudflare_Util_Settings as Settings;
+
 class Modules_DnsSyncCloudflare_Cloudflare_Record
 {
     public $id;
@@ -16,4 +19,39 @@ class Modules_DnsSyncCloudflare_Cloudflare_Record
     public $modified_on;
     public $data;
     public $priority;
+
+    /**
+     * @param PleskRecord $pleskRecord
+     * @return Modules_DnsSyncCloudflare_Cloudflare_Record
+     */
+    public static function fromPleskRecord(PleskRecord $pleskRecord)
+    {
+        $record = new self();
+        
+        $record->type = $pleskRecord->type;
+        $record->name = $pleskRecord->host;
+
+//        $record->proxied = (bool)Settings::useCloudflareProxy($pleskRecord->siteId, $pleskRecord->type);
+        $record->proxied = false;
+
+        $record->content = $pleskRecord->value;
+        $record->priority = '';
+
+        //Special record types
+        switch ($pleskRecord->type)
+        {
+            //Check for MX
+            case 'MX':
+                $record->priority = $pleskRecord->opt;
+                break;
+            //Check for SRV
+            case 'SRV':
+                $content = explode(' ', $pleskRecord->opt . ' ' . $pleskRecord->value);
+                $record->content = $content[1] . ' ' . $content[2] . ' ' . $content[3];
+                $record->priority = $content[0];
+                break;
+        }
+
+        return $record;
+    }
 }
