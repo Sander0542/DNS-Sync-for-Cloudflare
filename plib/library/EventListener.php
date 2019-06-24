3ef58a96feb6a3ff09a9ce8e2c099d2ff2b866e5
Sander Jochems <sanderjochems@hotmail.nl>
2019-06-24 12:42:25 +0200

    Create Automatic Update Listener

---
+++ plib/library/EventListener.php
@@ -0,0 +1,59 @@
+<?php
+
+use Modules_DnsSyncCloudflare_Util_Settings as Settings;
+use Modules_DnsSyncCloudflare_Cloudflare_Auth as CloudflareAuth;
+use Modules_DnsSyncCloudflare_Records_SyncRecord as SyncRecord;
+
+class Modules_DnsSyncCloudflare_EventListener implements EventListener
+{
+    /**
+     * @return array
+     */
+    public function filterActions()
+    {
+        return [
+            'domain_dns_update',
+        ];
+    }
+
+    /**
+     * @param $objectType
+     * @param $objectId
+     * @param $action
+     * @param $oldValues
+     * @param $newValues
+     */
+    public function handleEvent($objectType, $objectId, $action, $oldValues, $newValues)
+    {
+        switch ($action)
+        {
+            case 'domain_dns_update':
+                try
+                {
+                    $domain = pm_Domain::getByName($oldValues['Domain Name']);
+
+                    if (pm_Settings::get(Settings::getDomainKey(Settings::CLOUDFLARE_AUTO_SYNC, $domain), true))
+                    {
+                        $cloudflare = CloudflareAuth::login($domain);
+
+                        if ($cloudflare instanceof CloudflareAuth)
+                        {
+                            $removeOld = pm_Settings::get(Settings::getDomainKey(Settings::CLOUDFLARE_REMOVE_UNUSED, $domain), true);
+
+                            $records = SyncRecord::getRecords($domain, $cloudflare, $removeOld);
+
+                            foreach ($records as $record)
+                            {
+                                $record->syncRecord();
+                            }
+                        }
+                    }
+                }
+                catch (pm_Exception $e)
+                {
+                    //TODO: Error reporting
+                }
+                break;
+        }
+    }
+}
