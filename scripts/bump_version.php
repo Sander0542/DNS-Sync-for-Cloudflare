7b1294f3e33f62ef53a63ee5fa77535da387f033
Sander Jochems <sanderjochems@hotmail.nl>
2019-11-07 22:10:27 +0100

    Create bump_version.php

---
+++ scripts/bump_version.php
@@ -0,0 +1,27 @@
+<?php
+
+function bump_version($version)
+{
+    $metaFile = __DIR__ . '/../meta.xml';
+
+    $dom = new DOMDocument();
+    $dom->load($metaFile);
+
+    $root = $dom->documentElement;
+
+    $versionElm = $root->getElementsByTagName('version')->item(0);
+    $releaseElm = $root->getElementsByTagName('release')->item(0);
+
+    $versionElm->textContent = $version;
+
+    $release = $releaseElm->textContent;
+    $release++;
+    $releaseElm->textContent = $release;
+
+    if ($dom->save($metaFile) === false) {
+        throw new Exception("Could not update meta.xml");
+    }
+
+    echo "Version set to {$version}";
+    echo "Release set to {$release}";
+}
